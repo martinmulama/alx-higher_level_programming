93fc053f70c2f7b112eb550394ac5b94e7b3ac53
Martin Mulama <martin.mulama@yahoo.com>
2023-05-09 11:15:26 +0000

    added 6-print_comb3.py

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for i in range(9):
+    for j in range(10):
+        if j > i:
+            print("{:d}{:d}, ".format(i, j), end="")
+        if i == 8 and j == 9:
+            print("{:d}{:d}".format(i, j))
+            break
+
