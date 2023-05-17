d80273638df58a98203536d8c1c16cb896a69f80
Martin Mulama <martin.mulama@yahoo.com>
2023-05-17 09:51:56 +0000

    added 1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    list_copy = my_list.copy()
+
+    for i in range(len(list_copy)):
+        if list_copy[i] == search:
+            list_copy[i] = replace
+
+
+    return list_copy
