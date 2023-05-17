0a3f8047f2a920df142c43c11754cc6646218009
Martin Mulama <martin.mulama@yahoo.com>
2023-05-17 10:20:16 +0000

    added 2-uniq_add.py

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    unique_list = list(set(my_list))
+    result = 0
+
+    for i in range(len(unique_list)):
+        result += unique_list[i]
+
+    return result
