2d95e5ad7a1b05b19ac42e82c8ef03d67b36d140
Martin Mulama <martin.mulama@yahoo.com>
2023-05-29 10:10:08 +0000

    added 0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+
+    count = 0
+
+    try:
+        for element in my_list:
+            print(element, end='')
+            count += 1
+            if count == x:
+                break
+    except TypeError:
+        pass
+    finally:
+        print()
+        return count
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,1 @@
+This directory contains files on the conceptof error and exception handling in python.
