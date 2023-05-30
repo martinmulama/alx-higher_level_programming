7cb420cc1d573abfe2490bef9f3bbc28faabab66
Martin Mulama <martin.mulama@yahoo.com>
2023-05-30 10:40:46 +0000

    added 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Square module."""
+
+class Square:
+    """
+    This class represents a square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a Square instance.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
