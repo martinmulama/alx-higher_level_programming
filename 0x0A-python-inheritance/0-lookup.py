7b40e6e4ed42e6c74cb1f9ab3cc09d03612e75e0
Martin Mulama <martin.mulama@yahoo.com>
2023-06-12 11:01:59 +0000

    modified 0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -10,19 +10,6 @@ def lookup(obj):
     Returns:
         A list of strings representing the available attributes and methods of the object.
 
-    Example:
-        class Person:
-            def __init__(self, name, age):
-                self.name = name
-                self.age = age
-
-            def say_hello(self):
-                print(f"Hello, my name is {self.name}!")
-
-        person = Person("John", 25)
-        attributes_and_methods = lookup(person)
-        print(attributes_and_methods)
-        # Output: ['age', 'name', 'say_hello']
     """
     attributes = []
     methods = []
@@ -36,4 +23,3 @@ def lookup(obj):
                 attributes.append(attr)
 
     return attributes + methods
-
