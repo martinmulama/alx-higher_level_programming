480c4666c018ed9e0cbab40fa91cc6394b82aa65
Martin Mulama <martin.mulama@yahoo.com>
2023-06-12 11:12:45 +0000

    update repo

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,21 +1,20 @@
 #!/usr/bin/puthon3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    max_index = x - 1
-
     try:
-        for i in range(x):
-            if i > max_index:
-                raise IndexError("Index out of range")
-
+        for i, element in enumerate(my_list):
             try:
-                element = my_list[i]
                 print("{:d}".format(int(element)), end=' ')
                 count += 1
+                if count == x:
+                    break
             except ValueError:
                 pass
     except TypeError:
         pass
     finally:
         print()
+        if count < x:
+            print("{:d}".format(int(element)), end=' ')
+            raise IndexError("out of range")
         return count
