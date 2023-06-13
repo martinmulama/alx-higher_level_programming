5dd06b0d7dbe95a961a5bd4447356e5d7e0866e5
Martin Mulama <martin.mulama@yahoo.com>
2023-06-13 10:25:00 +0000

    added 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 # 0-read_file.py
 # Martin <martin.mulama@yahoo.com>
+""" Module that reads a file and prints its content """
+
 
 def read_file(filename=""):
-    """reads a text file (UTF8) and prints it to stdout"""
-
+    """ reads a text file (UTF8) and prints it to stdout """
     with open(filename, "r", encoding="utf-8") as file:
         print(file.read(), end="")
