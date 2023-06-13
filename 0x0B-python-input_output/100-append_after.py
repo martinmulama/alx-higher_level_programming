a992891be618e96c8c18e14e66ca210397aefd1d
Martin Mulama <martin.mulama@yahoo.com>
2023-06-13 14:52:43 +0000

    added 100-append_after.py

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Append after module """
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """ inserts a line of text to a file, after each line containing a specific string """
+    lines = []
+
+    with open(filename, "r") as file:
+        for line in file:
+            lines.append(line)
+            if search_string in line:
+                lines.append(new_string + "\n")
+
+    with open(filename, "w") as file:
+        file.writelines(lines)
