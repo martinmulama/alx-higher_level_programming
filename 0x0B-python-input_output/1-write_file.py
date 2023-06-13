d19d219ff4684e8ef4a325a2b26210fdc7e3c768
Martin Mulama <martin.mulama@yahoo.com>
2023-06-13 10:53:20 +0000

    updated 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,6 +4,13 @@
 """ Module that writes a string to a tct file and returns no of characters written """
 
 def write_file(filename="", text=""):
-    """ writes a string to a text file (UTF8) and returns the number of characters written """
+    """writes a string to a text file (UTF8) and returns the number of characters written.
+
+    Args:
+        filename: The name of file to open.
+        text: The text write in the file.
+    Returns:
+        Thhe number of characters in file
+    """
     with open(filename, "w", encoding="utf-8") as file:
         return file.write(text)
